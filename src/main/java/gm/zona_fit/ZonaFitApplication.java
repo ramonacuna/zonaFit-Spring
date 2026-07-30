9521a2dbb412d2629b0fdae882dbc17094df133e
Ramon Acuña <ramonacuna568@gmail.com>
2026-07-29 23:31:08 -0500

    Mejora de caracteristicas base y resolucion de casoss de usos detectados

--- src/main/java/gm/zona_fit/ZonaFitApplication.java
+++ src/main/java/gm/zona_fit/ZonaFitApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
 import java.util.Scanner;
 
 @SpringBootApplication
@@ -33,10 +34,10 @@ public class ZonaFitApplication implements CommandLineRunner {
 		while (!exitApp) {
 			var option = mostrarMenu(sc);
 			switch (option) {
-				case 1 -> clienteServicio.listarClientes().forEach(System.out::println);
+				case 1 -> listarClientes();
 				case 2 -> buscarCliente(sc);
 				case 3 -> agregarCliente(sc);
-				case 4 -> agregarCliente(sc);
+				case 4 -> modificarCliente(sc);
 				case 5 -> eliminarCliente(sc);
 				case 6 -> {
 					logger.info("Hasta Pronto!...");
@@ -63,40 +64,74 @@ public class ZonaFitApplication implements CommandLineRunner {
 
 	private static int mostrarMenu(Scanner sc) {
 		var menu = """
-                *** Zona Fit (GYM) ***
-                1. Listar Clientes
-                2. Buscar Cliente
-                3. Agregar Cliente
-                4. Modificar Cliente
-                5. Eliminar Cliente
-                6. Salir
-                Elija una Opcion:\s""";
+				\n*** Zona Fit (GYM) ***
+				1. Listar Clientes
+				2. Buscar Cliente
+				3. Agregar Cliente
+				4. Modificar Cliente
+				5. Eliminar Cliente
+				6. Salir
+				Elija una Opcion:\s\n""";
 		return leerEntero(sc, menu);
+	}
+
+	private void listarClientes() {
+		List<Cliente> listaClientes = clienteServicio.listarClientes();
+		logger.info("\n--- Listado de clientes ---\n");
+		for (Cliente cliente : listaClientes) {
+            logger.info("{}\n", cliente.toString());
+		}
 	}
 
 
 	private void buscarCliente(Scanner sc) {
-		Integer idCliente = leerEntero(sc,"Ingresa el ID del cliente: ");
-		logger.info(clienteServicio.buscarClientePorId(idCliente).toString());
+		Integer idCliente = leerEntero(sc, "Ingresa el ID del cliente: ");
+        if(clienteServicio.buscarClientePorId(idCliente) == null) {
+			logger.info("No existe el cliente con el ID: " + idCliente);
+		}else{
+			logger.info("\n{}\n", clienteServicio.buscarClientePorId(idCliente).toString());
+		}
 	}
 
 	private void agregarCliente(Scanner sc) {
-		Integer idCliente = leerEntero(sc,"Ingresa el ID del cliente: ");
 		logger.info("Ingresa el nombre del cliente: ");
 		String nombre = sc.nextLine();
 		logger.info("Ingresa el apellido del cliente: ");
 		String apellido = sc.nextLine();
 		logger.info("Ingresa el email del cliente: ");
-		Integer membresia = leerEntero(sc,"Ingresa el membresia: ");
+		Integer membresia = leerEntero(sc, "Ingresa el membresia: ");
+		Cliente cliente = new Cliente();
+		cliente.setNombre(nombre);
+		cliente.setApellido(apellido);
+		cliente.setMembresia(membresia);
+		clienteServicio.guardarCliente(cliente);
+        logger.info("\nCliente Agregado: {}\n", cliente.toString());
+	}
 
-		clienteServicio.guardarCliente(new Cliente(idCliente, nombre, apellido, membresia));
+	private void modificarCliente(Scanner sc) {
+		Integer idCliente = leerEntero(sc, "Ingresa el ID del cliente: ");
+		if(clienteServicio.buscarClientePorId(idCliente) == null) {
+			logger.info("Error: Cliente no encontrado: " + idCliente);
+		}else{
+			logger.info("Ingresa el nombre del cliente: ");
+			String nombre = sc.nextLine();
+			logger.info("Ingresa el apellido del cliente: ");
+			String apellido = sc.nextLine();
+			Integer membresia = leerEntero(sc, "Ingresa el membresia: ");
+			Cliente cliente = new Cliente(idCliente, nombre, apellido, membresia);
+			clienteServicio.guardarCliente(cliente);
+			logger.info("\n{}\n", cliente.toString());
+		}
 	}
 
 	private void eliminarCliente(Scanner sc) {
 		Integer idCliente = leerEntero(sc,"Ingresa el ID del cliente: ");
-		clienteServicio.borrarCliente(idCliente);
+		if(clienteServicio.buscarClientePorId(idCliente) == null) {
+			logger.info("No existe el cliente con el ID {}", idCliente);
+		}else{
+			logger.info("Cliente eliminado: {}\n",clienteServicio.buscarClientePorId(idCliente).toString());
+			clienteServicio.borrarCliente(idCliente);
+		}
+
 	}
-
-
-
 }
